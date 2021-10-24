4c1426d93f50ccf7dd465505dffa3bd574e97ef4
MamkinKinder <alex.fedkiv@gmail.com>
2021-10-24 11:50:01 +0300

    Task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -3,6 +3,6 @@ package it.campe;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Я думаю,быть программистом круто.");
+        System.out.println("Я думаю,быть программистом круто");
     }
 }
